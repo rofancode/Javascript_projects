c2189cf0f5887b2c56afe53de19ff313fcbc0038
rofancode <rofancode@gmail.com>
2023-02-21 14:35:58 +0900

    랜더링과 DB접속 추가

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,13 @@
 import '@/styles/globals.css'
+import Layout from '../components/layout'
 
 export default function App({ Component, pageProps }) {
   console.log('_app.js');
   // return <>Hello haha</>
   //return <Component {...pageProps} /> 
-  return <div style ={{border:'10px solid red'}}><Component {...pageProps} /> </div>
+  // return <div style ={{border:'10px solid red'}}><Component {...pageProps} /> </div>
   //app.js가 return 하는 것이 output이 되는 구나 // index.js가 component형태로 app의 매개변수로 들어옴
   // 모든 routing의 진입점. 글로벌하게 적용하는 곳
+  return <Layout><Component {...pageProps} /></Layout> 
+  // 이렇게 하면 페이지마다 Layout 에 감싸서 전달됨
 }
--- src/pages/create.js
+++ src/pages/create.js
@@ -1,10 +1,9 @@
-import Layout from '../components/layout';
 export default function Create() {
   return (
-    <Layout>
+    <>
       <h2>Create</h2>
-        Hello, create!!
-    </Layout>
+      Hello, create!!
+    </>
   )
 }
 
